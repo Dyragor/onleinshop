d6236e88849b09055bedcc346ed3ecb53e403030
5ia15anschranz <5ia15anschranz@gibwintra.ch>
2019-03-04 09:37:35 +0100

    Added Bootstrap

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import index from '../bootstrap-ecommerce-uikit/ui-ecommerce/index.html';
+
+const Home = (props) => {
+    return (
+        <div>
+            <index></index>
+        </div>
+    );
+};
